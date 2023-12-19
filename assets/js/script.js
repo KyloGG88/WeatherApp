3da16f7d25f7a62ac6ae6b82820306fad8aadcf7
Kyle GG <goliath.kylec@gmail.com>
2023-12-19 23:20:20 +0000

    api fetch success

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 const APIKey = "1cf3b6b2378dbd633c0e3d2ef94acefc";
 let buttonDiv = $(".button-div")
 let cityArr = []
+
+
+let city = $("#city-input").val()
+let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${APIKey}`
+
+fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
